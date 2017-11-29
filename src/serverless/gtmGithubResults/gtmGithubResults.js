31ac46e93b696d5c9fed0c063279afd819334c01
wyvern8 <wyvern@smartcreations.com.au>
2017-11-29 18:01:15 +1100

    feat(lambda): add results lambda starting point with sns event trigger

--- src/serverless/gtmGithubResults/gtmGithubResults.js
+++ src/serverless/gtmGithubResults/gtmGithubResults.js
@@ -1,1 +1,27 @@
-//placeholder
+'use strict';
+
+let json = require('format-json');
+let Consumer = require('sqs-consumer');
+
+function handle(event, context, callback) {
+
+    /* eslint-disable */
+    console.log('---------------------------------');
+    console.log(`Github-Results: `);
+    console.log('---------------------------------');
+    console.log('Payload', json.plain(event));
+    /* eslint-enable */
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            input: event,
+        }),
+    };
+
+    return callback(null, response);
+}
+
+module.exports = {
+    "handle": handle
+};
