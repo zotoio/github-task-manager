442ee73b90fb828ea6c23e02c7fac34387b5d3cd
wyvern8 <wyvern@smartcreations.com.au>
2017-12-13 20:38:44 +1100

    fix(executor): fix ping executor to send status per option count

--- src/executors/ExecutorPing.js
+++ src/executors/ExecutorPing.js
@@ -8,43 +8,39 @@ export class ExecutorPing extends Executor {
         super(eventData);
         this.options = this.getOptions();
 
-        this.runFunctions = {};
-        this.runFunctions['push'] = this.executeForPush;
-
     }
 
-    run(fn) {
-        return this.runFunctions[fn];
-    }
-
-    async executeForPush(task) {
+    async executeTask(task) {
 
         let count = parseInt(task.options.count);
 
+        let promises = [];
+
         for (let i = 1; i <= count; i++) {
+
             let status = Utils.createStatus(
                 this.eventData,
-                'success',
+                'pending',
                 'diagnostic',
                 `got ping ${i}`
             );
-            await Utils.postResultsAndTrigger(
+
+            promises.push(Utils.postResultsAndTrigger(
                 process.env.GTM_SQS_RESULTS_QUEUE,
                 status,
                 process.env.GTM_SNS_RESULTS_TOPIC,
                 'Ping').then(function () {
                 log.info(`sent ping ${i}`);
-            });
+            }));
+
+            await Utils.timeout(3000);
         }
 
-        let result = true;
-        log.info('Build Finished: ' + result);
-        return { passed: result, url: 'https://linux.die.net/man/8/ping' };
-    }
+        return Promise.all(promises).then(() => {
+            return Promise.resolve({ passed: true, url: `https://ping.io/ping/${task.options.count}` });
+        });
 
-    async executeTask(task) {
-        log.info('Task Ping started.');
-        return Promise.resolve({passed: true, url: 'https://ping.io/ping/' + task.options.count});
+
     }
 
 }
