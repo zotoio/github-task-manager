7ae6a865ccaa8cb033c36cd702dbeac85ab925ec
Silas Thomas <Silas.RobertThomas@nab.com.au>
2017-12-21 14:05:18 +1100

    revert changes - accidental commit

--- src/executors/ExecutorJenkins.js
+++ src/executors/ExecutorJenkins.js
@@ -1,84 +1,129 @@
-import { default as TeamCity } from 'teamcity-rest-api';
+import { default as JenkinsLib } from 'jenkins';
 import { Executor } from '../agent/Executor';
-import { Utils } from '../agent/AgentUtils';
-import { default as json } from 'format-json';
-let log = Utils.logger();
+import { AgentUtils } from '../agent/AgentUtils';
+let log = AgentUtils.logger();
 
-export class ExecutorTeamCity extends Executor {
+export class ExecutorJenkins extends Executor {
     constructor(eventData) {
         super(eventData);
         this.options = this.getOptions();
 
-        this.runFunctions = {};
-        this.runFunctions['pull_request'] = this.executeForPullRequest;
-
-        this.teamCity = TeamCity.create({
-            url: this.options.GTM_TEAMCITY_URL,
-            username: this.options.GTM_TEAMCITY_USER,
-            password: this.options.GTM_TEAMCITY_PASSCODE
+        this.jenkins = JenkinsLib({
+            baseUrl: AgentUtils.formatBasicAuth(
+                this.options.GTM_JENKINS_USER,
+                this.options.GTM_JENKINS_TOKEN,
+                this.options.GTM_JENKINS_URL
+            ),
+            crumbIssuer: true,
+            promisify: true
         });
     }
 
-    run(fn) {
-        return this.runFunctions[fn];
-    }
+    taskNameToBuild(context) {
+        let desiredTask = context;
+        console.debug(desiredTask);
 
-    async executeForPullRequest(task) {
-        log.info('TeamCity Build Finished');
-        log.debug(task);
-        return { passed: true, url: this.options.GTM_TEAMCITY_URL + '/viewType.html?buildTypeId=' + jobName };
+        let tasks = {
+            functional: 'EXECUTE_AUTOMATED_TESTS',
+            a11y: 'EXECUTE_AUTOMATED_TESTS'
+        };
 
-    }
-
-    createTeamCityBuildNode(task, jobName) {
-        var buildProperties = '';
-        for(var buildProperty in task.options) {
-            buildProperties = buildProperties + '<property name="' + buildProperty + '" value="' + task.options[buildProperty] + '"/>\n';
+        if (!tasks.hasOwnProperty(desiredTask)) {
+            log.error('No Tasks Matched Request: ' + desiredTask);
+            return null;
+        } else {
+            let mappedTask = tasks[desiredTask];
+            log.info('Mapped Task ' + desiredTask + ' to Job ' + mappedTask);
+            return mappedTask;
         }
+    }
 
-        let buildNodeObject = '<build>\n' +
-            '<buildType id="' + jobName + '" />\n' +
-            '<properties>\n' +
-                buildProperties +  
-            '</properties>\n' + 
-            '</build>';
+    async waitForBuildToExist(buildName, buildNumber) {
+        return new Promise(async (resolve, reject) => {
+            let exists = false;
+            let maxRetries = 600;
+            let tries = 0;
+            while (!exists && tries++ < maxRetries) {
+                exists = await this.jenkins.build
+                    .get(buildName, buildNumber)
+                    .then(
+                        function() {
+                            log.info(
+                                `Build ${buildName} #${buildNumber} Started!`
+                            );
+                            return true;
+                        },
+                        async function() {
+                            log.debug(
+                                `Build ${buildName} #${buildNumber} Hasn't Started: ${tries}`
+                            );
+                            await AgentUtils.timeout(10000);
+                            return false;
+                        }
+                    );
+            }
+            exists ? resolve(true) : reject();
+        });
+    }
 
-        return buildNodeObject;
+    async waitForBuild(buildName, buildNumber) {
+        let buildDict = await this.jenkins.build
+            .get(buildName, buildNumber)
+            .then(function(data) {
+                return data;
+            });
+        let tries = 1;
+        while (buildDict.result === null) {
+            await AgentUtils.timeout(5000);
+            buildDict = await this.jenkins.build
+                .get(buildName, buildNumber)
+                .then(function(data) {
+                    log.debug(
+                        `Waiting for Build '${buildName}' to Finish: ${tries++}`
+                    );
+                    return data;
+                });
+        }
+        log.info(
+            `Build Finished: ${buildName} #${buildNumber} - ${buildDict.result}`
+        );
+        return buildDict;
+    }
+
+    createJenkinsBuildParams(task) {
+        // todo jenkins params by eventType, task options etc
+        return {
+            TAGS: JSON.stringify(task.options.tags),
+            ENVIRONMENT: 'automated-test-env'
+        };
     }
 
     async executeTask(task) {
-
-        let jobName = task.context;
-
+        let jobName = this.taskNameToBuild(task.context);
         if (jobName == null) {
-            await Utils.timeout(4000);
+            await AgentUtils.timeout(4000);
             return 'NO_MATCHING_TASK';
         }
 
-        log.info('TeamCity Project[' + jobName + '] Started.');
+        let buildParams = this.createJenkinsBuildParams(task);
+        log.debug(buildParams);
 
-        let buildNode = this.createTeamCityBuildNode(task, jobName);
-        log.debug('TeamCity Project[' + jobName + '] buildNode : ' + buildNode);
-                
-        this.teamCity.builds.startBuild(buildNode)
-        .then(function(buildStatus) {
-                console.log(buildStatus.id);
-                this.teamCity.builds.get(buildStatus.id)
-                .then(function(_build) {
-                    while(!_build.state === 'finished') {
-                      console.log(_build.state);  
-                    }
-                    console.log(_build.statistics);
-                });
+        log.info('Starting Jenkins Job: ' + jobName);
+        let buildNumber = await this.jenkins.job.build({
+            name: jobName,
+            parameters: buildParams
         });
+        let buildExists = await this.waitForBuildToExist(jobName, buildNumber);
+        console.debug(buildExists);
+        let result = await this.waitForBuild(jobName, buildNumber);
 
-        //to-do
-        //once build is finished, get the statistics from [this.options.GTM_TEAMCITY_URL + _build.statistics] and post the results
-
-        let result = true;
-        log.info('Build Finished: ' + result);
-        return { passed: result, url: this.options.GTM_TEAMCITY_URL + '/viewType.html?buildTypeId=' + jobName };
+        let resultBool = result.result === 'SUCCESS';
+        return Promise.resolve({
+            passed: resultBool,
+            url: result.url,
+            message: `${jobName} #${buildNumber} - ${result.result}`
+        }); // todo handle results
     }
 }
 
-Executor.register('TeamCity', ExecutorTeamCity);
+Executor.register('Jenkins', ExecutorJenkins);
