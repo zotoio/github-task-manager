cae5bbb4968c4ce65a60157cfee4c90df1334d38
wyvern8 <wyvern@smartcreations.com.au>
2017-12-22 22:37:22 +1100

    test(plugin): unit tests

---
+++ test/agent/Plugin.spec.js
@@ -0,0 +1,37 @@
+import { describe, it } from 'mocha';
+import { default as assert } from 'assert';
+import { Plugin } from '../../src/agent/Plugin';
+
+describe('Plugin', function() {
+    describe('register', function() {
+        it('should register new class', function() {
+            class Animal extends Plugin {}
+            class Dog extends Animal {}
+
+            Animal.register('Dog', Dog);
+
+            assert.equal(Animal.isRegistered('Dog'), true);
+        });
+    });
+
+    describe('create', function() {
+        it('should create new instance of class', function() {
+            class Animal extends Plugin {}
+            class Dog extends Animal {
+                constructor(options) {
+                    super();
+                    this.options = options;
+                }
+                puppies() {
+                    return this.options.puppies;
+                }
+            }
+
+            Animal.register('Dog', Dog);
+
+            let dalmation = Animal.create('Dog', { puppies: 101 });
+
+            assert.equal(dalmation.puppies(), 101);
+        });
+    });
+});
