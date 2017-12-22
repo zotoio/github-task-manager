2d6edc6661689798ccd1ab0cd8b164195cd70ce6
wyvern8 <wyvern@smartcreations.com.au>
2017-12-22 23:02:55 +1100

    test(executor and eventhandler superclasses):

---
+++ test/agent/Executor.spec.js
@@ -0,0 +1,25 @@
+import { default as fs } from 'fs';
+import { describe, it, beforeEach } from 'mocha';
+import { default as assert } from 'assert';
+import { Plugin } from '../../src/agent/Plugin';
+import { Executor } from '../../src/agent/Executor';
+
+describe('Executor', function() {
+    let executor;
+    beforeEach(() => {
+        let eventData = JSON.parse(fs.readFileSync(__dirname + '/../fixtures/githubEventPayload.json', 'utf-8'));
+        executor = new Executor(eventData);
+    });
+
+    describe('constructor', function() {
+        it('should instantiate as Plugin', function() {
+            assert.equal(executor instanceof Plugin, true);
+        });
+    });
+
+    describe('getOptions', function() {
+        it('should return environment', function() {
+            assert.equal(executor.getOptions(), process.env);
+        });
+    });
+});
