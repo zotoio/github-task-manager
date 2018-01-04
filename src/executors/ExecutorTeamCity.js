de81e786c4387768f05e888db3e8ec7ba611c63a
Silas D Thomas <silas.jonathan@gmail.com>
2018-01-05 09:32:41 +1100

    feat(TeamCity Executor): TeamCity Executor implementation
    TeamCity Executor implementation with status update

--- src/executors/ExecutorTeamCity.js
+++ src/executors/ExecutorTeamCity.js
@@ -1,53 +1,102 @@
-//import { default as TeamCity } from 'teamcity-rest-api';
+import { default as TeamCity } from 'teamcity-rest-api';
 import { Executor } from '../agent/Executor';
 import { AgentUtils } from '../agent/AgentUtils';
-import { default as json } from 'format-json';
 let log = AgentUtils.logger();
+
+/**
+ * Sample .githubTaskManager.json task config
+ * {
+        "executor": "TeamCity",
+        "context": "projectName",
+        "options": {
+          "build_tag" : "proj_wasX",
+          "build_env" : "env_dev",
+          "cuke_env" : "env_dev|staging|prod",
+          "cuke_tags" : [
+            "@proj_wasX",
+            "@proj_wasn"
+          ]
+        }
+    }
+*/
 
 export class ExecutorTeamCity extends Executor {
     constructor(eventData) {
         super(eventData);
         this.options = this.getOptions();
 
-        this.runFunctions = {};
-        this.runFunctions['pull_request'] = this.executeForPullRequest;
-
-        /*this.teamCity = TeamCity.create({
-            url: 'http://localhost:8111',
-            username: 'user',
-            password: 'pass'
-        });*/
+        this.teamCity = TeamCity.create({
+            url: this.options.GTM_TEAMCITY_URL,
+            username: this.options.GTM_TEAMCITY_USER,
+            password: this.options.GTM_TEAMCITY_PASSCODE
+        });
     }
 
-    run(fn) {
-        return this.runFunctions[fn];
-    }
+    createTeamCityBuildNode(task, jobName) {
+        var buildProperties = '';
+        for(var buildProperty in task.options) {
+            buildProperties = buildProperties + '<property name="' + buildProperty + '" value="' + task.options[buildProperty] + '"/>\n';
+        }
 
-    async executeForPullRequest(task) {
-        //let jobName = this.taskNameToBuild(taskName);
-
-        log.info(`teamcity options: ${json.plain(task.options)}`);
-
-        /*
-        let buildNodeObject = '<build>' +
-            '<buildType id="TestConfigId" />' +
+        let buildNodeObject = '<build>\n' +
+            '<buildType id="' + jobName + '" />\n' +
+            '<properties>\n' +
+                buildProperties +  
+            '</properties>\n' + 
             '</build>';
 
-        this.teamCity.builds.startBuild(buildNodeObject)
-            .then(function(buildStatus) {
-                log.debug(buildStatus.id);
-            });
-            */
-
-        let result = true;
-        log.info('Build Finished: ' + result);
-        return { passed: result, url: 'https://www.jetbrains.com/teamcity' };
+        return buildNodeObject;
     }
 
     async executeTask(task) {
-        log.info('TeamCity Build Finished');
-        log.debug(task);
-        return { passed: true, url: 'https://www.jetbrains.com/teamcity' };
+
+        let jobName = task.context;
+
+        if (jobName == null) {
+            await AgentUtils.timeout(4000);
+            return 'NO_MATCHING_TASK';
+        }
+
+        let buildNode = this.createTeamCityBuildNode(jTask, jobName);
+        
+        let teamCityBuildId = await this.teamCity.builds.startBuild(buildNode);
+        log.info(`TeamCity Project[${jobName}] with buildNumber : ${teamCityBuildId.id} Started.`);
+
+        let completedBuild = await this.waitForBuildToComplete(jobName, teamCityBuildId.id);
+        log.info(`TeamCity Project[${jobName}] with buildNumber : #${teamCityBuildId.id} finished with status : ${completedBuild.status}`);
+
+        //TO-DO: parse the following parameters from the statistics result xml feed, "FailedTestCount", "PassedTestCount", "TotalTestCount"
+        //let stats = this.getBuildStatistics(this.options.GTM_TEAMCITY_URL+completedBuild.statistics, jobName, teamCityBuildId.id);
+
+        let result = completedBuild.status === 'SUCCESS';
+        return { passed: result, url: completedBuild.buildType.webUrl };
+
+    }
+
+    async waitForBuildToComplete(buildName, buildNumber) {
+        let buildDict = await this.teamCity.builds.get(buildNumber);
+
+        let tries = 1;
+        while (buildDict.state !== 'finished') {
+            await AgentUtils.timeout(5000);
+            buildDict = await this.teamCity.builds.get(buildNumber).then(function(data) {
+                log.debug(`Waiting for Build '${buildName}' to Finish: ${tries++}`);
+                return data;
+            });
+        }
+        log.debug(`TeamCity Project[${buildName}] with buildNumber : #${buildNumber} : ${buildDict.state}`);
+        return buildDict;
+    }
+
+    getBuildStatistics(statsUrl, buildName, buildNumber) {
+
+        log.debug(statsUrl);
+
+        request(statsUrl, { json: true }, (err, res, body) => {
+            if (err) { return log.error(err); }
+            log.debug(body.url);
+            log.debug(body.explanation);
+        });
     }
 }
 
