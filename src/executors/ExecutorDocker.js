5a43d2cc91eb4e4a0f9fd59cbbf78a2131015e36
wyvern8 <wyvern@smartcreations.com.au>
2018-02-05 22:36:53 +1100

    fix(docker): tidy executor towards result capture

--- src/executors/ExecutorDocker.js
+++ src/executors/ExecutorDocker.js
@@ -97,66 +97,7 @@ export class ExecutorDocker extends Executor {
 
         let docker = new Docker();
 
-        /**
-         * Get logs from running container
-         */
-        function containerLogs(container) {
-            let logBuffer = [];
-
-            // create a single stream for stdin and stdout
-            let logStream = new stream.PassThrough();
-            logStream.on('data', function(chunk) {
-                logBuffer.push(chunk.toString('utf8')); // todo find a better way
-                if (logBuffer.length % 100 === 0) {
-                    log.info(logBuffer.reverse().join(''));
-                    logBuffer = [];
-                }
-            });
-
-            container.logs(
-                {
-                    follow: true,
-                    stdout: true,
-                    stderr: true
-                },
-                function(err, stream) {
-                    if (err) {
-                        return log.error(err.message);
-                    }
-                    container.modem.demuxStream(stream, logStream, logStream);
-                    stream.on('end', function() {
-                        log.info(logBuffer.reverse().join(''));
-                        logBuffer = [];
-                        logStream.end('!stop!');
-                    });
-                }
-            );
-        }
-
-        function pullImage(image) {
-            return new Promise((resolve, reject) => {
-                docker.pull(image, function(err, stream) {
-                    if (err) {
-                        return reject(err);
-                    }
-
-                    docker.modem.followProgress(stream, onFinished, onProgress);
-
-                    function onFinished(err, output) {
-                        if (err) {
-                            return reject(err);
-                        }
-                        return resolve(output);
-                    }
-
-                    function onProgress(event) {
-                        log.info(event);
-                    }
-                });
-            });
-        }
-
-        return pullImage(image)
+        return this.pullImage(docker, image)
             .then(() => {
                 return docker.createContainer({
                     Image: image,
@@ -170,7 +111,7 @@ export class ExecutorDocker extends Executor {
             })
 
             .then(container => {
-                return containerLogs(container);
+                return this.containerLogs(container);
             })
 
             .then(() => {
@@ -198,6 +139,65 @@ export class ExecutorDocker extends Executor {
                 task.results = resultSummary;
                 return Promise.reject(task);
             });
+    }
+
+    /**
+     * Get logs from running container
+     */
+    containerLogs(container) {
+        let logBuffer = [];
+
+        // create a single stream for stdin and stdout
+        let logStream = new stream.PassThrough();
+        logStream.on('data', function(chunk) {
+            logBuffer.push(chunk.toString('utf8'));
+            if (logBuffer.length % 100 === 0) {
+                log.info(logBuffer.reverse().join(''));
+                logBuffer = [];
+            }
+        });
+
+        container.logs(
+            {
+                follow: true,
+                stdout: true,
+                stderr: true
+            },
+            function(err, stream) {
+                if (err) {
+                    return log.error(err.message);
+                }
+                container.modem.demuxStream(stream, logStream, logStream);
+                stream.on('end', function() {
+                    log.info(logBuffer.reverse().join(''));
+                    logBuffer = [];
+                    logStream.end('!stop!');
+                });
+            }
+        );
+    }
+
+    pullImage(docker, image) {
+        return new Promise((resolve, reject) => {
+            docker.pull(image, function(err, stream) {
+                if (err) {
+                    return reject(err);
+                }
+
+                docker.modem.followProgress(stream, onFinished, onProgress);
+
+                function onFinished(err, output) {
+                    if (err) {
+                        return reject(err);
+                    }
+                    return resolve(output);
+                }
+
+                function onProgress(event) {
+                    log.info(event);
+                }
+            });
+        });
     }
 }
 
